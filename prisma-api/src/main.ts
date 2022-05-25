9d951d793d59b2a8b354b7dc450ca017696628e2
Brian Pontes <brian.robert16@hotmail.com>
2022-05-25 20:32:11 -0300

    trabalhando com intercepetors

--- prisma-api/src/main.ts
+++ prisma-api/src/main.ts
@@ -1,3 +1,6 @@
+import { NotFoundInterceptor } from './common/errors/interceptors/notfound.interceptor';
+import { UnauthorizedInterceptor } from './common/errors/interceptors/unauthorized.interceptor';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
@@ -11,6 +14,9 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new UnauthorizedInterceptor());
+  app.useGlobalInterceptors(new NotFoundInterceptor());
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
