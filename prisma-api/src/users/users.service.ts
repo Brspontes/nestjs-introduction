bb36db327b90f805243783dc148af97b2c838c3b
Brian Pontes <brian.robert16@hotmail.com>
2022-05-25 18:31:53 -0300

    realizando crud prisma

---
+++ prisma-api/src/users/users.service.ts
@@ -0,0 +1,28 @@
+import { UsersRepository } from './repositories/users.repository';
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly usersRepository: UsersRepository) {}
+  async create(createUserDto: CreateUserDto) {
+    return await this.usersRepository.create(createUserDto);
+  }
+
+  async findAll() {
+    return await this.usersRepository.findAll();
+  }
+
+  async findOne(id: number) {
+    return await this.usersRepository.findOne(id);
+  }
+
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    return await this.usersRepository.update(id, updateUserDto);
+  }
+
+  async remove(id: number) {
+    return await this.usersRepository.remove(id);
+  }
+}
